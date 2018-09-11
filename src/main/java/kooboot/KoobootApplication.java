fbe5c01afa3bf8b65182ae2c7a83492fbe742403
songintae <songintae92@hanmail.net>
2018-09-11 17:37:11 +0900

    Configuration : initalize application config
    1. setting application.yml
    2. setting logback config

---
+++ src/main/java/kooboot/KoobootApplication.java
@@ -0,0 +1,12 @@
+package kooboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoobootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoobootApplication.class, args);
+    }
+}
